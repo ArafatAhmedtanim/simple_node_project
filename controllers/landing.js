cdaa57f6c4447b3c754eab3d2f941ddddf6ba5e5
arafatgitlab <arafatgitlab.gmail.com>
2020-01-29 12:52:51 +0600

    redirect

--- controllers/landing.js
+++ controllers/landing.js
@@ -1,3 +1,9 @@
 exports.get_landing = function (req, res, next) {
     res.render('landing', { title: 'Express' })
 }
+
+exports.submit_lead = function (req, res, next) {
+    email = req.body.lead_email // WTF !! How i get this email in my template
+    res.redirect('/')
+}
+
